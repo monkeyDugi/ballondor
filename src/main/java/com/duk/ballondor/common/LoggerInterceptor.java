8485aefc35bd9987ad260654c824e2f38317555b
monkeyDugi <lbd4946@gmail.com>
2019-09-18 00:06:18 +0900

    while의 무한루프에 빠질 수 있는 단점을 피하기 위해 for문 사용

--- src/main/java/com/duk/ballondor/common/LoggerInterceptor.java
+++ src/main/java/com/duk/ballondor/common/LoggerInterceptor.java
@@ -1,6 +1,5 @@
 package com.duk.ballondor.common;
 
-import java.util.Collections;
 import java.util.Enumeration;
 
 import javax.servlet.http.HttpServletRequest;
@@ -22,7 +21,7 @@ public class LoggerInterceptor extends HandlerInterceptorAdapter{
 		log.debug("URL [{}]," + request.getRequestURI());
 		
 		Enumeration paramNames = request.getParameterNames();
-		while(paramNames.hasMoreElements()) {
+		for(int i=0; paramNames.hasMoreElements(); i++) {
 			
 			String key   = (String)paramNames.nextElement();
 			String value = request.getParameter(key); 
